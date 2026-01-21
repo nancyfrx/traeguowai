7bca04d65d2c30fe6710bc310f65ab578d20e832
nancyfrx <1226712554@qq.com>
2026-01-21 17:09:27 +0800

    增加批量采纳用例功能2

--- set_env.sh
+++ set_env.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # AI API Keys
-export ZHIPU_API_KEY="00b08fee86f84b33a353f45b0b0f6c0f.tBYJiYISieVqrTjA"
-export DEEPSEEK_API_KEY="sk-01b8e4b7af0f4edcb008e9ea46757846"
+export ZHIPU_API_KEY=""
+export DEEPSEEK_API_KEY=""
 
 # Mail Configuration
-export SMTP_USERNAME="fengruxue01@163.com"
-export SMTP_PASSWORD="WR55xCjn4QfvFk9G"
+export SMTP_USERNAME=""
+export SMTP_PASSWORD=""
 
 # Database Configuration
-export DB_USERNAME="root"
-export DB_PASSWORD="123456"
+export DB_USERNAME=""
+export DB_PASSWORD=""
